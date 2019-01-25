aa61cf977b4c76c8e93984b7bedce279a4795004
Andrei <andrey.laptev@outlook.com>
2019-01-25 16:33:04 -0500

    Level 1 - Sky added, horiz movement added

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float speed = 7f;
+
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        // Listen for Space to jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //rBody.gravityScale = -2;
+            rBody.AddForce(new Vector2(0, 500));
+        }
+
+        // Flipping NOT WORKING SO FAR
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) )
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        //float vert = Input.GetAxis("Vertical"); // Not good - gravity pulls player down 
+        Debug.Log(horiz);
+        rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+    }
+}
