005187c8fc9ddbb2c2e7cbdfdd71f5d54797a5be
Andrei <andrey.laptev@outlook.com>
2019-01-25 18:29:09 -0500

    Added Level 1 Title and buttons bound

---
+++ Assets/Scripts/Level1_Title_Delay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class Level1_Title_Delay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed");
+            Application.LoadLevel(3);
+        }
+
+        //Debug.Log("1 run");
+
+        //Thread.Sleep(5000); //Delay of Title screen for 3 sec
+
+        //Debug.Log("2 run");
+
+        //
+    }
+
+}
