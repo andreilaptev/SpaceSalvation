b38273b1554d0d8fc7832f3773cac6183574b5c4
Andrei <andrey.laptev@outlook.com>
2019-01-25 15:32:05 -0500

    test

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public int nextPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            Application.LoadLevel(nextPage);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        }
+    }
+}
---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class StartController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thread.Sleep(2000);
+
+        Application.LoadLevel(1);
+    }
+}
