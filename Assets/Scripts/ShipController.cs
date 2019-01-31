fbb2e61428e934b19482c21b19b9d59721adaece
Andrei <andrey.laptev@outlook.com>
2019-01-31 11:11:41 -0500

    "Colliding with asteroid" event created

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,94 +4,79 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+
+    // VARIABLES
     public float initialSpeed = 7f;
     float xCoord;
     private Rigidbody2D rBody;
 
     private float speed;
 
+    Collider2D coll = new Collider2D();
+
+
+    // METHODS
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        
     }
 
-    private void Update()
-    {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            //Debug.Log("LLL");
-
-            //float horiz = Input.GetAxis("Horizontal");
-            //rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
-        }
-
-
-        // Listen for Space to jump
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //rBody.gravityScale = -2;
-            rBody.AddForce(new Vector2(0, 500));
-        }
-
-        // Flipping NOT WORKING SO FAR
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-
-
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) )
-        {
-            
-        }
-
-    }
+  
 
     // Update is called once per frame
     void FixedUpdate()
     {
      
-        float horiz = Input.GetAxis("Horizontal");
-      
+        ////////////////////////////////
+        /// Ship's Movement
+        float horiz = Input.GetAxis("Horizontal");      
 
         xCoord = rBody.position.x;
 
-        speed = initialSpeed;
-         
+        speed = initialSpeed;         
 
+        // Checking Out Of Bounds
         if (xCoord < - 8) 
-        {
-            
+        {            
             if (horiz > 0)
-            {
-               
-                speed = initialSpeed;
-               
+            {               
+                speed = initialSpeed;               
             }else
             {
                 speed = 0;
-            }          
-  
+            }        
         };
 
         if (xCoord > 8)
         {
             if (horiz < 0)
             {
-                speed = initialSpeed;
-              
+                speed = initialSpeed;              
             }
             else
             {
                 speed = 0;
             }
-        };
-
-        
+        };        
             
             rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
-        
+        ////////////////////////////////
+        /// END OF Ship's Movement
+        /// 
+              
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Asteroid")
+        {
+            Debug.Log("HIT");
+        }
         
     }
+
+ 
 }
