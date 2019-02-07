539e2d9ac0a1d6d89936ec50e6627bd88c46cd70
Keshok <keshok@live.ca>
2019-02-06 22:25:23 -0500

    Added audio assets and audio source for some scenes, and made changes to Credits scene
    Added audio assets. Implemented audio tracks for Start, Menu, Die, and Level1_Post_Title scenes. Made adjustments to CreditsScene.

---
+++ Assets/Scripts/CombatMusicControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class CombatMusicControl : MonoBehaviour
+{
+
+    public AudioMixerSnapshot outOfCombat;
+    public AudioMixerSnapshot inCombat;
+    public AudioClip[] laserBeams;
+    public AudioSource laserBeamSource;
+    public float bpm = 128;
+
+    private float m_TransitionIn;
+    private float m_TransitionOut;
+    private float m_QuarterNote;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_QuarterNote = 60 / bpm;
+        m_TransitionIn = m_QuarterNote;
+        m_TransitionOut = m_QuarterNote * 32;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CombatZone"))
+        {
+            inCombat.TransitionTo(m_TransitionIn);
+            PlayLaserBeam();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("CombatZone"))
+        {
+            outOfCombat.TransitionTo(m_TransitionOut);
+        }
+    }
+
+    void PlayLaserBeam()
+    {
+        int randClip = Random.Range(0, laserBeams.Length);
+        laserBeamSource.clip = laserBeams[randClip];
+        laserBeamSource.Play();
+    }
+}
