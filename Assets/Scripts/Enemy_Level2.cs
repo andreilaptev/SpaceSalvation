a033474f72659040bd148ee4024f9ed1ca7513b0
Andrei <andrey.laptev@outlook.com>
2019-03-10 13:59:35 -0400

    Level 2: enemy targeting Added

---
+++ Assets/Scripts/Enemy_Level2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Level2 : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        targetPlayer();
+        
+    }
+
+    private void targetPlayer()
+    {
+        //Var1
+        //this.transform.LookAt(player.transform);
+
+        //Var2
+        //transform.LookAt(Vector3.forward, Vector3.Cross(Vector3.forward, player.transform.position - this.transform.position));
+
+        //Var3
+
+        float angle = 0;
+
+        Vector3 relative = transform.InverseTransformPoint(player.transform.position);
+        angle = Mathf.Atan2(relative.x, relative.y) * Mathf.Rad2Deg;
+        transform.Rotate(0, 0, -angle);
+    }
+}
