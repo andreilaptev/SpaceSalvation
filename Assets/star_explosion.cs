c4763298c0e55fcc50cc0f5360447cd34884f7f4
Andrei <bokken2008@gmail.com>
2019-03-26 11:29:29 -0400

    Coin explosion prefab added

---
+++ Assets/star_explosion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class star_explosion : MonoBehaviour
+{
+    public float time = 0.5f;
+
+    // Update is called once per frame
+    void Start()
+    {
+        Destroy(this.gameObject, time);
+    }
+}
