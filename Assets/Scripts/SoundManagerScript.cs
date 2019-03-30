13af4017fdf7b94e548a54b36a7148197ec52058
Keshok Sivalingam <keshok@live.ca>
2019-03-30 17:41:36 -0400

    Removed WebGL_Build01 folder, added a few more audio assets, and modified ShipController script
    Removed the aforementioned directory to eliminate unnecessary storage on GitHub repository. Modified ShipController script to adhere the modern changes, enforced by Unity, by changing Application.LoadLevel() [now an obsolete function call) to SceneManager.LoadScene() function.

---
+++ Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static AudioClip fireSound, enemyFireSound, playerDeathSound, enemyDeathSound, explosionSound, collectCoinSound, collectHealthSound;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireSound = Resources.Load<AudioClip> ("laser1");
+        enemyFireSound = Resources.Load<AudioClip> ("");
+        playerDeathSound = Resources.Load<AudioClip> ("");
+        enemyDeathSound = Resources.Load<AudioClip> ("");
+        explosionSound = Resources.Load<AudioClip> ("");
+        collectCoinSound = Resources.Load<AudioClip> ("retro-collect-item-sfx");
+        collectHealthSound = Resources.Load<AudioClip>("");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound (string clip)
+    {
+        switch (clip)
+        {
+            case "fire":
+                audioSrc.PlayOneShot(fireSound);
+                break;
+            case "enemyFire":
+                audioSrc.PlayOneShot(enemyFireSound);
+                break;
+            case "playerDeath":
+                audioSrc.PlayOneShot(playerDeathSound);
+                break;
+            case "enemyDeath":
+                audioSrc.PlayOneShot(enemyDeathSound);
+                break;
+            case "explosion":
+                audioSrc.PlayOneShot(explosionSound);
+                break;
+            case "collectCoin":
+                audioSrc.PlayOneShot(collectCoinSound);
+                break;
+            case "collectHealth":
+                audioSrc.PlayOneShot(collectHealthSound);
+                break;
+        }
+    }
+}
