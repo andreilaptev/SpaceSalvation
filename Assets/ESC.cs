fd74abb7093064a948aec419d33d59253c351292
Victor Dallarosa Teixeira <victordalt@hotmail.com>
2019-04-11 22:02:40 -0400

    Back Button and ESC Working.

---
+++ Assets/ESC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESC : MonoBehaviour
+{
+
+    public GameObject MainMenu;
+    public GameObject OptionsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MainMenu.SetActive(true);
+            OptionsMenu.SetActive(false);
+        }
+    }
+}
